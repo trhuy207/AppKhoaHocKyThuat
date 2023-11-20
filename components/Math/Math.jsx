402a20f2865d11d389108ec971d4c0c4d32ca84a
williamtran207 <quochuyphbrvt07@gmail.com>
2023-11-20 19:16:13 +0700

    update

--- components/Math/Math.jsx
+++ components/Math/Math.jsx
@@ -1,12 +1,21 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Pdf from 'react-native-pdf'
 
 import styles from './Math.style'
 
 const Math = () => {
+    const source = { uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf', cache: true };
     return (
         <View style={styles.container}>
-            <Text>Toán</Text>
+            <Pdf
+                trustAllCerts={false}
+                source={PdfResource}
+                style={styles.pdf}
+                onLoadComplete={(numberOfPages, filePath) => {
+                    console.log(`number of pages: ${numberOfPages}`);
+                }}
+            />
         </View>
     )
 }
--- components/Math/Math.style.js
+++ components/Math/Math.style.js
@@ -3,6 +3,12 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex:1
+    },
+    
+    pdf: {
+        flex:1,
+        width:Dimensions.get('window').width,
+        height:Dimensions.get('window').height,
     }
 }) 
 
